04417233af85995a328f45fc8b1c20e24653398c
sahilshivekar <sahilshivekarce@gmail.com>
2024-10-01 04:00:51 +0530

    Implement changes to make auth.middleware.js for both user and ngo

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-// import { User } from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 import { NGO } from "../models/ngo.model.js";
 import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
@@ -7,28 +7,34 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const verifyJwt = asyncHandler(async (req, _, next) => {
     const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    const UserAccessToken = req.cookies?.UserAccessToken || req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!accessToken) {
-        throw new ApiError(401, "Unauthorized access");
-    }
+    if (accessToken) {
+        const decodedUser = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 
-    const decodedUser = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-
-    if (decodedUser.registrationNumber) {
         const ngo = await NGO.findOne({ registrationNumber: decodedUser.registrationNumber }).select("-password");
+
         if (!ngo) {
             throw new ApiError(401, "Invalid access token for NGO");
         }
         req.ngo = ngo;
+
+        next();
+    } else if (UserAccessToken) {
+        const decodedUser = jwt.verify(UserAccessToken, process.env.ACCESS_TOKEN_SECRET);
+        
+        const user = await User.findById(decodedUser._id).select("-password");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid access token for user");
+        }
+        req.user = user;
+
+        next();
     } else {
-        // const user = await User.findById(decodedUser._id).select("-password");
-        // if (!user) {
-        //     throw new ApiError(401, "Invalid access token for User");
-        // }
-        // req.user = user;
+        throw new ApiError(401, "Unauthorized access");
     }
 
-    next();
 });
 
 
